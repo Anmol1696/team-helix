e3227230045ba393129a1d32e34fc4467ab5167b
gmakestyle <stylegmake@gmail.com>
2023-03-26 00:22:03 +0900

    [REBASE] Add frontend code

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,86 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { 
+  ConnectWallet, 
+  useAddress,
+  useBalance,
+  useSigner,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { useState } from "react";
+import { NATIVE_TOKEN_ADDRESS } from "@thirdweb-dev/sdk";
 import "./styles/Home.css";
 
 export default function Home() {
+  
+  // Set constants incl. contract address, user address, and amount to stake state variables
+  const stakingContractAddress = "0x3D5568dBc683B199Bef5E329Ae88d52AfdDb8564";
+  const address = useAddress();
+  const [amountToStake, setAmountToStake] = useState<string>("");
+  const abi = [
+    "function depositTransaction(address to, uint256 value, uint64 gasLimit, bool isCreation, bytes data) payable returns ()"
+  ]
+  const signer = useSigner();
+  const contract = new ethers.Contract(stakingContractAddress, abi, signer);
+
+  // Set options for the contract call
+  const gasLimit = 155000;
+  let value = ethers.utils.parseEther('0');
+  if (amountToStake) {
+    value = ethers.utils.parseEther(amountToStake);
+  }
+  const options = {
+    value: value,
+    gasLimit: gasLimit,
+  }
+
+  // Get token balances. TODO: Get staked token balance
+  const { data: stakingTokenBalance, isLoading: isBalanceLoading} 
+    = useBalance(NATIVE_TOKEN_ADDRESS);
+  
   return (
     <div className="container">
       <main className="main">
         <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+          Welcome to l2staking!
         </h1>
-
         <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.tsx</code>, then modify the{" "}
-          <code className="code">src/App.tsx</code> file!
+          Stake some ETH!
         </p>
 
-        <div className="connect">
+        <div>
           <ConnectWallet />
         </div>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+        <div className={"stakeContainer"}>
+        <label>
+        Amount of ETH to stake:
+          <input
+            className="textbox"
+            type="number"
+            value={amountToStake}
+            onChange={(e) => setAmountToStake(e.target.value)}
+          />
+          </label>
+        </div>
+        <div className = "stake">
+          <button onClick=
+            {() => contract.depositTransaction(
+                address,
+                ethers.utils.parseEther(amountToStake),
+                gasLimit,
+                false,
+                ethers.constants.HashZero,
+                options
+              )
+            }
+            >
+              Stake
+        </button> 
+        </div>
+        <div className="grid" id="center">
+          <button className="card">
+            <h2>Native token balance</h2>
+            <p>{isBalanceLoading ? 0 : stakingTokenBalance?.displayValue}</p>
+          </button>
         </div>
       </main>
     </div>
